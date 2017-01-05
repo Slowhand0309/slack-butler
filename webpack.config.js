725956554dc4cf60ec29c0a310a835cef10e8d7e
Slowhand0309 <slowhand0309@gmail.com>
2017-01-06 01:20:02 +0900

    Add calendar integrate

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path');
 
+require('dotenv').config();
+
 module.exports = {
   context: __dirname + '/lib',
   entry: './entry.js',
@@ -15,6 +17,18 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         query: {
           presets: 'es2015'
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'string-replace',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          multiple: [
+            { search: '$TIME_ZONE', replace: process.env.TIME_ZONE },
+            { search: '$CALENDAR_ID', replace: process.env.CALENDAR_ID },
+            { search: '$EVENT_NOTIFY_TITLE', replace: process.env.EVENT_NOTIFY_TITLE }
+          ]
         }
       }
     ]
